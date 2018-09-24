eb2378961614fd9b471db916874981eec8ef8717
EC2 Default User <ec2-user@ip-172-31-21-4.us-east-2.compute.internal>
2018-09-24 22:42:26 +0000

    HW2-1.1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,40 +11,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @all_ratings = Movie.all_ratings
-
-    unless params[:ratings].nil?
-      @checked_ratings = params[:ratings]
-      session[:checked_ratings] = @checked_ratings
-    end
-
-    if params[:sort_by].nil?
-    else
-      session[:sort_by] = params[:sort_by]
-    end
-
-    if params[:sort_by].nil? && params[:ratings].nil? && session[:checked_ratings]
-      @checked_ratings = session[:checked_ratings]
-      @sorti_by = session[:sort_by]
-      flash.keep
-      redirect_to movies_path({order_by: @sort_by, ratings: @checked_ratings})
-    end
-
     @movies = Movie.all
-
-    if session[:checked_ratings]
-      @movies = @movies.select{ |movie| session[:checked_ratings].include? movie.rating }
-    end
-
-    if session[:sort_by] == "title"
-      @movies = @movies.sort! { |a,b| a.title <=> b.title }
-      @movie_highlight = "hilite"
-    elsif session[:sort_by] == "release_date"
-      @movies = @movies.sort! { |a,b| a.release_date <=> b.release_date }
-      @date_highlight = "hilite"
-    else
-      
-    end
   end
 
   def new
