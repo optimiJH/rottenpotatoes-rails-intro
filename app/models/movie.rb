8550920fcb31f6db79b308a39741483208a2a11a
EC2 Default User <ec2-user@ip-172-31-21-4.us-east-2.compute.internal>
2018-09-25 20:06:28 +0000

    part 3 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ActiveRecord::Base
-  attr_accessible :title, :rating, :description, :release_date
+  #attr_accessible :title, :rating, :description, :release_date
   def self.all_ratings
     a = Array.new
     self.select("rating").uniq.each {|x| a.push(x.rating)}
